8246cd031bc8714350b5eb06b2047dd52d1319d6
hydroflame <79246165+hydroflame@users.noreply.github.com>
2023-10-13 15:17:20 -0400

    Update PlantTimeDisplay.tsx

--- src/PlantTimeDisplay.tsx
+++ src/PlantTimeDisplay.tsx
@@ -1,7 +1,5 @@
-// PlantTimeDisplay.tsx
-
-import React from 'react';
-import { Box, Grid, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 interface PlantTimeProps {
   plantTime: {
@@ -16,26 +14,23 @@ interface PlantTimeProps {
 const PlantTimeDisplay: React.FC<PlantTimeProps> = ({ plantTime }) => {
   return (
     <div className="plant-time-display">
-                      <Box 
-                    border={1} 
-                    borderColor="grey.300" 
-                    borderRadius={2}
-                    width="320px"
-                    padding={1}
-                    margin= "0 auto"
-                >
-      <Grid container spacing={0.5} alignItems="center">
-        <Grid item><Typography>Year: {plantTime.year}|</Typography></Grid>
-        <Grid item><Typography>Season: {plantTime.season}|</Typography></Grid>
-        <Grid item><Typography>Day: {plantTime.day}|</Typography></Grid>
-        
-        <Grid item>
-            <Typography>
-                Hour: {plantTime.hour.toString().padStart(2, '0')}:{plantTime.update_counter.toString().padStart(2, '0')}
-            </Typography>
-        </Grid>
-
-      </Grid>
+      <Box
+        border={1}
+        borderColor="grey.300"
+        borderRadius={2}
+        width="320px"
+        padding={1}
+        margin="0 auto"
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Typography>Year: {plantTime.year}</Typography>
+        <Typography>Season: {plantTime.season}</Typography>
+        <Typography>Day: {plantTime.day}</Typography>
+        <Typography>
+          Hour: {plantTime.hour.toString().padStart(2, "0")}:
+          {plantTime.update_counter.toString().padStart(2, "0")}
+        </Typography>
       </Box>
     </div>
   );
