2dff8c5d687c40c6fa782e443d2c49f242dd13f1
Andrew Hase <andrew.hase@gmail.com>
2023-10-23 20:07:27 -0400

    Auto-increment version number

--- src/store.ts
+++ src/store.ts
@@ -16,7 +16,7 @@ import { runMigrations } from "./migrations";
 
 const persistedState: RootState | undefined = loadState();
 //Should be auto incremented when there is a push
-export const currentVersion = 39;
+export const currentVersion = 40;
 
 // Check if the flag isNewUser exists in local storage
 let isNewUser = localStorage.getItem("isNewUser");
